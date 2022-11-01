1388ea7c772a0c1534ec8c0543d712c42b234287
Aether Kerstens <philippekerstens@gmail.com>
2022-11-01 08:15:54 -0700

    Create candidate spec skeleton

---
+++ spec/candidate_spec.rb
@@ -0,0 +1,26 @@
+require './lib/candidate'
+
+RSpec.describe Candidate do
+  before(:each)do
+    diana = Candidate.new({name: "Diana D", party: :democrat})
+  end
+  it '#initialize' do
+
+  end
+
+  it 'has a name' do
+
+  end
+
+  it 'has a party end' do
+
+  end
+
+  it 'can hold votes' do
+
+  end
+
+  it 'can collect votes' do
+
+  end
+end
