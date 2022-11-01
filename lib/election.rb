373f2f12adca7a64e8c92458fe19d401348ac9bb
Aether Kerstens <philippekerstens@gmail.com>
2022-11-01 09:54:40 -0700

    Refactor Election

--- lib/election.rb
+++ lib/election.rb
@@ -1,9 +1,10 @@
 class Election
-  attr_reader :year, :races
+  attr_reader :year, :races, :candidates
 
   def initialize(year)
     @year = year
     @races = []
+    @candidates = []
   end
 
   def add_race(race)
